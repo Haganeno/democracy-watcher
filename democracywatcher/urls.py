206b3ae3034a1dcdb4a1c4c7225344e94ae8b9fe
Olivier Da Rocha <darochaolivier@gmail.com>
2023-04-08 22:47:30 +0200

    Initial commit

---
+++ democracywatcher/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+]
