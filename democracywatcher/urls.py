bac22365a7d73fc2da28d12e6b8e06fd9ffb6134
Olivier Da Rocha <darochaolivier@gmail.com>
2023-04-08 22:53:27 +0200

    Move manage.py

--- democracywatcher/urls.py
+++ democracywatcher/urls.py
@@ -6,6 +6,6 @@ from rest_framework import routers
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-api/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
